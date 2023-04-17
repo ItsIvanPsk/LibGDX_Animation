a0f6b507f389d7982e1d8feb0e72b171cd03d1cc
Ivan Figueredo <ivanfigueredo.et@gmail.com>
2023-04-17 17:37:14 +0200

    Second Animation

--- core/src/com/mygdx/animation_ivan/ControllerAnimation.java
+++ core/src/com/mygdx/animation_ivan/ControllerAnimation.java
@@ -7,32 +7,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.mygdx.animation_ivan.utils.AppConstants;
 
 public class ControllerAnimation implements ApplicationListener {
 	Animation<TextureRegion> walk_animation;
-	Texture walk_sheet;
+	Texture player;
 	SpriteBatch batch;
 	float state_time;
+	public TextureRegion[] frames = new TextureRegion[6];
+	public float stateTime = 0f;
+	Animation<TextureRegion> playerAnim;
+
 	@Override
 	public void create() {
-		walk_sheet = new Texture(Gdx.files.internal("sprite_sheet.png"));
+		player = new Texture(Gdx.files.internal("spritesheet.png"));
 
-		TextureRegion[][] txt_region = TextureRegion.split(
-				walk_sheet,
-				walk_sheet.getWidth() / AppConstants.FRAME_COLS,
-				walk_sheet.getHeight() / AppConstants.FRAME_ROWS
-		);
+		// per cada frame cal indicar x,y,amplada,alçada
+		frames[0] = new TextureRegion(player, 86, 87, 26, 30);
+		frames[1] = new TextureRegion(player, 111, 87, 18, 30);
+		frames[2] = new TextureRegion(player, 129, 86, 19, 31);
+		frames[3] = new TextureRegion(player, 149, 87, 25, 29);
+		frames[4] = new TextureRegion(player, 203, 87, 22, 30);
+		frames[5] = new TextureRegion(player, 227, 87, 18, 30);
+		playerAnim = new Animation<>(0.20f, frames);
 
-		TextureRegion[] walkFrames = new TextureRegion[AppConstants.FRAME_COLS * AppConstants.FRAME_ROWS];
-		int index = 0;
-		for (int it = 0; it < AppConstants.FRAME_ROWS; it++) {
-			for (int it2 = 0; it2 < AppConstants.FRAME_COLS; it2++) {
-				walkFrames[index++] = txt_region[it][it2];
-			}
-		}
-
-		walk_animation = new Animation<>(0.05f, walkFrames);
 		batch = new SpriteBatch();
 		state_time = 0f;
 	}
@@ -43,10 +40,16 @@ public class ControllerAnimation implements ApplicationListener {
 	@Override
 	public void render() {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		state_time += Gdx.graphics.getDeltaTime();
-		TextureRegion currentFrame = walk_animation.getKeyFrame(state_time, true);
+		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+		TextureRegion frame = playerAnim.getKeyFrame(stateTime, true);
+
 		batch.begin();
-		batch.draw(currentFrame,100, 100, 400, 400);
+		batch.draw(frame,
+				200, 100,
+				0,0,
+				frame.getRegionWidth(),
+				frame.getRegionHeight(),
+				5,5,0);
 		batch.end();
 	}
 
@@ -59,6 +62,6 @@ public class ControllerAnimation implements ApplicationListener {
 	@Override
 	public void dispose() {
 		batch.dispose();
-		walk_sheet.dispose();
+		player.dispose();
 	}
 }
--- core/src/com/mygdx/animation_ivan/utils/AppConstants.java
+++ core/src/com/mygdx/animation_ivan/utils/AppConstants.java
@@ -3,6 +3,6 @@ package com.mygdx.animation_ivan.utils;
 public class AppConstants {
 
     public static final int FRAME_COLS = 6;
-    public static final int FRAME_ROWS = 5;
+    public static final int FRAME_ROWS = 1;
 
 }
